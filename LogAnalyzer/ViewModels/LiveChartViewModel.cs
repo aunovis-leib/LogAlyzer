189b8598ea15a7d7a26c2b957d9f8352aa59f194
Leib <Leib@aunovis.de>
2026-02-26 21:10:25 +0100

    added section

--- LogAnalyzer/ViewModels/LiveChartViewModel.cs
+++ LogAnalyzer/ViewModels/LiveChartViewModel.cs
@@ -3,6 +3,7 @@ using LiveCharts.Wpf;
 using LogAnalyzer.Models;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Media;
 
 namespace LogAnalyzer.ViewModels
 {
@@ -10,28 +11,46 @@ namespace LogAnalyzer.ViewModels
     {
         public LogType? TypeToShow { get; set; }
 
-        private SeriesCollection _chartSeries = [];
+        private SeriesCollection _chartSeries = new SeriesCollection();
         public SeriesCollection ChartSeries
         {
             get => _chartSeries;
             set { _chartSeries = value; OnPropertyChanged(); }
         }
+        private SectionsCollection _sections = new SectionsCollection();
+        public SectionsCollection Sections
+        {
+            get => _sections;
+            set { _sections = value; OnPropertyChanged(); }
+        }
 
-        private IList<string> _xLabels = [];
+        private IList<string> _xLabels = new List<string>();
         public IList<string> XLabels
         {
             get => _xLabels;
             set { _xLabels = value; OnPropertyChanged(); }
         }
 
+
         public LiveChartViewModel()
         {
-            ChartSeries = [];
-            XLabels = [];
+            ChartSeries = new SeriesCollection();
+            XLabels = new List<string>();
+            Sections = new SectionsCollection();
         }
 
         public void UpdateFromEntries(IEnumerable<LogFileEntry> entries, DateTime? fromDate, DateTime? toDate)
         {
+            // Avoid running chart-building logic in the Visual Studio XAML designer
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                // ensure collections exist for the designer
+                ChartSeries = new SeriesCollection();
+                XLabels = new List<string>();
+                Sections = new SectionsCollection();
+                return;
+            }
+
             var filtered = entries.Where(e =>
                 (fromDate is null || e.Date.Date >= fromDate.Value.Date) &&
                 (toDate is null || e.Date.Date <= toDate.Value.Date));
@@ -86,6 +105,27 @@ namespace LogAnalyzer.ViewModels
             }
 
             ChartSeries = series;
+            // Build a single section (width = 1 hour): highlight the last hour that contains an Error
+            var sections = new SectionsCollection();
+            int lastErrorIndex = -1;
+            for (int i = 0; i < byHour.Count; i++)
+            {
+                if (byHour[i].Any(e => e.Type == LogType.Error))
+                    lastErrorIndex = i;
+            }
+
+            if (lastErrorIndex >= 0)
+            {
+                sections.Add(new AxisSection
+                {
+                    Value = lastErrorIndex,
+                    SectionWidth = 2.0,
+                    Fill = new SolidColorBrush(Color.FromArgb(40, 255, 0, 0)),
+                    Stroke = Brushes.Transparent
+                });
+            }
+
+            Sections = sections;
         }
 
         private static System.Windows.Media.SolidColorBrush GetColor(LogType logType)
